3da0cc16edc67f9721ed0a06cbc88c4061a909b9
Gatin <gatinslaughter@gmail.com>
2026-05-10 17:21:40 -0400

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 and not set -q TMUX
-  tmux new-session -A -s cozy
+  tmux new-session -A -s 1337
 end
 
 fish_vi_key_bindings
