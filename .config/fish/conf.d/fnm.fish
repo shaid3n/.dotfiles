7c841de0436c83281f19b76c1459d311908be3af
Gatin <gatinslaughter@gmail.com>
2026-09-20 04:23:52 -0400

    update fish config

--- .config/fish/conf.d/fnm.fish
+++
@@ -1,7 +0,0 @@
-
-# fnm
-set FNM_PATH "/home/shaid3n/.local/share/fnm"
-if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-  fnm env | source
-end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,37 +1,22 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
 and not set -q TMUX
   tmux new-session -A -s 1337
 end
 
 fish_vi_key_bindings
-# fish_default_key_bindings
 set fish_greeting
 starship init fish | source
 fzf --fish | source
 zoxide init fish --cmd cd | source
 mise activate fish | source
 eval (dircolors -c ~/.config/dircolors/.dircolors | string replace "setenv" "set -gx")
+source ~/.config/fish/themes/theme.fish
 
 alias v=nvim
 alias vconf="nvim ~/.dotfiles/.config/nvim"
 alias lg=lazygit
 alias pn=pnpm
-alias ls="eza --color=always --group-directories-first"
+alias ls="eza --color=auto --group-directories-first"
 alias ll="eza -lah --git"
 
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
 ln -sf /mnt/wslg/runtime-dir/wayland-0 /run/user/1000/wayland-0 2>/dev/null
-
-# pnpm
-set -gx PNPM_HOME "/home/shaid3n/.local/share/pnpm"
-if not string match -q -- "$PNPM_HOME/bin" $PATH
-  set -gx PATH "$PNPM_HOME/bin" $PATH
-end
-# pnpm end
